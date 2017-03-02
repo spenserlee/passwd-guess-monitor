bc8971b14f43b606f606256364d74b3110845191
Spenser Lee <Spenser.L159@gmail.com>
2017-03-02 15:48:07 -0800

    added base gui

--- passwd-guess-monitor/mainwindow.cpp
+++ passwd-guess-monitor/mainwindow.cpp
@@ -6,9 +6,117 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initUi();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initUi()
+{
+    ui->infoTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    QStringList logFiles;
+
+    logFiles << "/var/log/auth.log";
+    logFiles << "/var/log/secure";
+
+    ui->logFile->addItems(logFiles);
+
+
+}
+
+void MainWindow::on_unblockBtn_clicked()
+{
+    // execute iptable command to remove user chain for ip blocks
+}
+
+void MainWindow::on_registerBtn_clicked()
+{
+    // thread to monitor file in /opt/passwd-guess-monitor/activity.log
+
+    // get user settings
+
+    // execute script
+
+    // add script to crontab
+
+}
+
+void MainWindow::on_resetHrs_valueChanged(int arg1)
+{
+    resetHr = arg1;
+    if (arg1 == 24)
+    {
+        ui->resetMins->setValue(0);
+        resetMin = 0;
+    }
+    else if (arg1 == 0 && resetMin == 0)
+    {
+        ui->resetMins->setValue(1);
+        resetMin = 1;
+    }
+}
+
+void MainWindow::on_resetMins_valueChanged(int arg1)
+{
+    resetMin = arg1;
+
+    if (arg1 == 60)
+    {
+        resetHr += 1;
+        ui->resetHrs->setValue(resetHr);
+        ui->resetMins->setValue(0);
+        resetMin = 0;
+    }
+    else if (arg1 == 0 && resetHr == 0)
+    {
+        ui->resetMins->setValue(1);
+        resetMin = 1;
+    }
+    else if (resetHr == 24)
+    {
+        ui->resetMins->setValue(0);
+        resetMin = 0;
+    }
+}
+
+void MainWindow::on_blockHrs_valueChanged(int arg1)
+{
+    blockHr = arg1;
+    if (arg1 == 24)
+    {
+        ui->blockMins->setValue(0);
+        blockMin = 0;
+    }
+    else if (arg1 == 0 && blockMin == 0)
+    {
+        ui->blockMins->setValue(1);
+        blockMin = 1;
+    }
+}
+
+void MainWindow::on_blockMins_valueChanged(int arg1)
+{
+    blockMin = arg1;
+
+    if (arg1 == 60)
+    {
+        blockHr += 1;
+        ui->blockHrs->setValue(blockHr);
+        ui->blockMins->setValue(0);
+        blockMin = 0;
+    }
+    else if (arg1 == 0 && blockHr == 0)
+    {
+        ui->blockMins->setValue(1);
+        blockMin = 1;
+    }
+    else if (blockHr == 24)
+    {
+        ui->blockMins->setValue(0);
+        blockMin = 0;
+    }
 }
--- passwd-guess-monitor/mainwindow.h
+++ passwd-guess-monitor/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDebug>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,30 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void initUi();
+
+    void on_unblockBtn_clicked();
+
+    void on_registerBtn_clicked();
+
+    void on_resetHrs_valueChanged(int arg1);
+
+    void on_resetMins_valueChanged(int arg1);
+
+    void on_blockHrs_valueChanged(int arg1);
+
+    void on_blockMins_valueChanged(int arg1);
+
 private:
     Ui::MainWindow *ui;
+
+    int resetHr = 0;
+    int resetMin = 0;
+    int blockHr = 0;
+    int blockMin = 0;
+
 };
 
 #endif // MAINWINDOW_H
